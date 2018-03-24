1014eaa164ef526c5aee4cc30c33bebcc48529d2
Neriel <nerielnexus@gmail.com>
2018-03-25 06:12:29 +0900

    패킷 구조에 대한 생각과 해당 내용 추가 WSABUF 에 강제로 집어넣을 구조체 2개

--- Server_Client/Server/ClientSession.h
+++ Server_Client/Server/ClientSession.h
@@ -38,6 +38,25 @@ struct OverlappedIOContext
 	WSABUF			wsaBuf;
 };
 
+struct ConnectionPacket
+{
+	CONNECTION_EVENT	conEvent;		// 4byte
+	unsigned int		userid;			// 4byte
+	short				side;			// 2byte, padding 2byte
+	short				pattern1;
+	short				pattern2;
+	short				pattern3;
+};										// total size = 4*2 + (2+2)*4 = 24byte
+
+struct ObjectPacket
+{
+	OBJECT_EVENT	objEvent;			// 4byte
+	unsigned int	mainObj;
+	unsigned int	subObj;
+	float			xpos;				// 4byte
+	float			ypos;
+};										// total size = 4*5 = 20byte
+
 class ClientSession
 {
 public:
